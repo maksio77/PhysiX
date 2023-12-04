96dcb95e1c3ccf205997e2ce905bf47fb9605673
Maksym Pavliv <maksiopavliv0@gmail.com>
2023-12-04 10:06:08 +0200

    add .gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 
+
 function App() {
   const user = localStorage.getItem("token");
 
