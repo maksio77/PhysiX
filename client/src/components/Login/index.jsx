0e11dc971a25b41b3cd597566d40165556461963
Maksym Pavliv <maksiopavliv0@gmail.com>
2023-12-28 10:31:04 +0200

    add logo and styles

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import logo from '../../images/logo192.png'
 
 const Login = () => {
   const [data, setData] = useState({
@@ -34,13 +35,15 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className='flex flex-col sm:flex-row bg-white border-gray-100 shadow-xl rounded-xl mx-auto p-4 sm:p-8'>
-        <div className="justify-center bg-teal-500 rounded-xl flex p-4 sm:p-8 flex-col mb-4 sm:mb-0">
+    <div className="flex justify-center items-center min-h-screen flex-col sm: w-auto">
+      <img src={logo} alt={'LOGO'} className='scale-50 p-0'/>
+      <div className='flex flex-col sm:flex-row bg-white border-gray-100 shadow-xl rounded-xl p-4 sm:p-14'>
+        <div className="justify-center bg-teal-500 rounded-xl flex p-4 flex-col mb-4 w-full sm:w-auto">
           <form className='flex flex-col mx-auto' onSubmit={handleSubmit}>
             {/*<FaRegUser color='white'/>*/}
-            <h1 className='mx-auto text-4xl justify-center flex mb-5 text-white'>Login to Your Account</h1>
-            <div className='flex'>
+            <h1 className='mx-auto text-4xl justify-center flex mb-5 text-white sm: text-2xl'>Login to Your
+              Account</h1>
+            <div className='flex sm: ml-10'>
               <MdOutlineEmail color='white' size={25} className='flex m-3 mx-auto justify-center'/>
               <input
                   type="text"
@@ -49,30 +52,31 @@ const Login = () => {
                   onChange={handleChange}
                   value={data.email}
                   required
-                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5 ml-1.5'
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5 ml-1.5 sm: w-64'
               ></input>
             </div>
-            <div className='flex'>
+            <div className='flex sm: ml-10'>
               <RiLockPasswordLine color='white' size={25} className='flex m-3 mx-auto justify-center'/>
               <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                onChange={handleChange}
-                value={data.password}
-                required
-                className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5 ml-1.5'
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={handleChange}
+                  value={data.password}
+                  required
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5 ml-1.5 sm: w-64 mx-auto'
               ></input>
             </div>
             <Link
-              to="/forgot-password"
-              style={{
-                alignSelf: "flex-start",
-                textDecoration: "none",
-                color: "white",
-              }}
+                to="/forgot-password"
+                style={{
+                  alignSelf: "flex-start",
+                  textDecoration: "none",
+                  color: "white",
+                }}
+                className='sm: ml-10'
             >
-              <p style={{ padding: "0 15px" }}>Forgot Password?</p>
+              <p style={{padding: "0 15px"}}>Forgot Password?</p>
             </Link>
             {error && <div className='bg-red-400 mx-auto w-96 rounded-md p-2 mt-1'>{error}</div>}
             <button type="submit" className="text-xl bg-white text-black rounded-xl p-2 m-3 mx-auto">
--- client/src/components/Signup/index.jsx
+++ client/src/components/Signup/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import logo from "../../images/logo192.png";
 
 const Signup = () => {
   const [data, setData] = useState({
@@ -41,64 +42,65 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className='flex flex-col sm:flex-row bg-white border-gray-100 shadow-xl rounded-xl mx-auto p-4 sm:p-8'>
-        <div className="justify-center bg-teal-500 rounded-xl flex p-4 sm:p-8 flex-col mb-4 sm:mb-0">
-          <h1 className="text-white text-4xl items-center mb-8">Welcome Back</h1>
-          <Link to="/login">
-            <button type="button" className="flex text-black text-xl bg-white rounded-xl p-2 mx-auto">
-              Sign in
-            </button>
-          </Link>
-        </div>
-        <div className='p-4 sm:p-16 flex-grow'>
-          <form className='flex flex-col mx-auto' onSubmit={handleSubmit}>
-            <h1 className='mx-auto text-4xl justify-center flex mb-5'>Create Account</h1>
-            <input
-              type="text"
-              placeholder="First Name"
-              name="firstName"
-              onChange={handleChange}
-              value={data.firstName}
-              required
-              className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
-            ></input>
-            <input
-              type="text"
-              placeholder="Last Name"
-              name="lastName"
-              onChange={handleChange}
-              value={data.lastName}
-              required
-              className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
-            ></input>
-            <input
-              type="text"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-              className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
-            ></input>
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
-              required
-              className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
-            ></input>
-            {error && <div className='bg-red-400 mx-auto w-96 rounded-md p-2 mt-1'>{error}</div>}
-            {msg && <div className='bg-green-400 mx-auto rounded-md p-2'>{msg}</div>}
-            <button type="submit" className="text-xl bg-teal-500 text-white rounded-xl p-2 m-3 mx-auto">
-              Sign Up
-            </button>
-          </form>
+      <div className="flex justify-center items-center min-h-screen flex-col">
+        <img src={logo} alt={'LOGO'} className='scale-50 p-0'/>
+        <div className='flex flex-col sm:flex-row bg-white border-gray-100 shadow-xl rounded-xl mx-auto p-4 sm:p-8'>
+          <div className="justify-center bg-teal-500 rounded-xl flex p-4 sm:p-8 flex-col mb-4 sm:mb-0">
+            <h1 className="text-white text-4xl items-center mb-8">Welcome Back</h1>
+            <Link to="/login">
+              <button type="button" className="flex text-black text-xl bg-white rounded-xl p-2 mx-auto">
+                Sign in
+              </button>
+            </Link>
+          </div>
+          <div className='p-4 sm:p-16 flex-grow'>
+            <form className='flex flex-col mx-auto' onSubmit={handleSubmit}>
+              <h1 className='mx-auto text-4xl justify-center flex mb-5'>Create Account</h1>
+              <input
+                  type="text"
+                  placeholder="First Name"
+                  name="firstName"
+                  onChange={handleChange}
+                  value={data.firstName}
+                  required
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
+              ></input>
+              <input
+                  type="text"
+                  placeholder="Last Name"
+                  name="lastName"
+                  onChange={handleChange}
+                  value={data.lastName}
+                  required
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
+              ></input>
+              <input
+                  type="text"
+                  placeholder="Email"
+                  name="email"
+                  onChange={handleChange}
+                  value={data.email}
+                  required
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
+              ></input>
+              <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={handleChange}
+                  value={data.password}
+                  required
+                  className='bg-gray-100 outline-0 rounded-md p-3 w-96 mb-1.5'
+              ></input>
+              {error && <div className='bg-red-400 mx-auto w-96 rounded-md p-2 mt-1'>{error}</div>}
+              {msg && <div className='bg-green-400 mx-auto rounded-md p-2'>{msg}</div>}
+              <button type="submit" className="text-xl bg-teal-500 text-white rounded-xl p-2 m-3 mx-auto">
+                Sign Up
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
