4cbaab5c36dde7f6535cf60f91fb8d89572ecb42
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-01-24 18:37:43 +0200

    perfect blocks

--- client/src/components/Theme/index.jsx
+++ client/src/components/Theme/index.jsx
@@ -23,17 +23,31 @@ const Theme = () => {
 
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-            <div className="flex flex-wrap justify-center items-center gap-4 max-w-screen-xl mx-auto mt-20 mb-8">
-                {theme.map((item) => (
-                    <div key={item._id} className="bg-white rounded-lg overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 shadow-md flex-grow">
-                        <div className="p-4">
-                            <h3 className="text-xl">{replaceUnderscores(item.text, item.formulas)}</h3>
-                        </div>
-                    </div>
-                ))}
-            </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto mt-28 mb-8">
+            {theme.map((item) => (
+              <div key={item._id} className="bg-white rounded-lg overflow-hidden shadow-md">
+                <div className="p-8">
+                  <h3 className="text-xl">{replaceUnderscores(item.text, item.formulas)}</h3>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
     );
+
+    // return (
+    //     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    //         <div className="flex flex-wrap justify-center items-center gap-4 max-w-screen-xl mx-auto mt-28 mb-8">
+    //             {theme.map((item) => (
+    //                 <div key={item._id} className="bg-white rounded-lg overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 shadow-md flex-grow">
+    //                     <div className="p-4">
+    //                         <h3 className="text-xl">{replaceUnderscores(item.text, item.formulas)}</h3>
+    //                     </div>
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default Theme;
