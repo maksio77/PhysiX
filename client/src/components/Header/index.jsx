f4498c25926323ec3d71b4c0663ef53c2f0f772c
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-01-25 10:37:05 +0200

    logout link header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     const handleLogout = () => {
         localStorage.removeItem("token");
@@ -9,10 +11,18 @@ const Header = () => {
             <nav className="bg-teal-500 p-4 w-full fixed top-0 z-10 flex justify-between shadow-md">
                 <h1 className="text-4xl text-white">PhysiX</h1>
                 <button className="bg-white text-teal-500 px-4 py-2 rounded" onClick={handleLogout}>
+                    <Link
+                        to={'/login'}
+                        style={{
+                        alignSelf: "flex-start",
+                        textDecoration: "none",
+                        }}
+                    >
                     Logout
+                    </Link>
                 </button>
             </nav>
-      </>
+        </>
     )
 }
 
