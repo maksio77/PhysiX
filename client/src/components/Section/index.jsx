55f37b9d7336c8bc9b8873d7d01bb368395f5f82
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-01-25 13:07:19 +0200

    useLocation test

--- client/src/components/Section/index.jsx
+++ client/src/components/Section/index.jsx
@@ -1,49 +1,60 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 const Section = () => {
-    const [themes, setThemes] = useState([]);
-    const param = useParams();
+  const [themes, setThemes] = useState([]);
+  const param = useParams();
 
-    useEffect(() => {
-        const getSection = async () => {
-        try {
-            const url = `http://localhost:4000/api/sections/${param.section}`;
-            const { data } = await axios.get(url);
-            setThemes(data.themes);
-        } catch (error) {   
-            console.log(error);
-            setThemes([]);
-        }
-        };
-        getSection();
-    },[param.section]);
-    
-    return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-            <div className="flex flex-wrap justify-center items-center gap-4 max-w-screen-xl mx-auto mt-20 mb-8">
-                {themes.map((theme) => (
-                    <div key={theme._id} className="bg-white rounded-lg overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 shadow-md flex-grow">
-                        {/* <img src="https://via.placeholder.com/300" alt="section" className="w-full h-48 object-cover" /> */}
-                        <div className="p-4">
-                            <Link
-                                to={`/sections/${param.section}/${theme.themeRoute}`}
-                                style={{
-                                alignSelf: "flex-start",
-                                textDecoration: "none",
-                                }}
-                                className='sm: ml-10'
-                            >
-                                <h3 className="text-lg font-semibold">{theme.themeName}</h3>
-                            </Link>
-                        </div>
-                        
-                    </div>
-                ))}
+  useEffect(() => {
+    const getSection = async () => {
+      try {
+        const url = `http://localhost:4000/api/sections/${param.section}`;
+        const { data } = await axios.get(url);
+        setThemes(data.themes);
+      } catch (error) {
+        console.log(error);
+        setThemes([]);
+      }
+    };
+    getSection();
+  }, [param.section]);
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <h2 className="text-4xl font-semibold mx-auto mt-28">Розділ</h2>
+      <div className="flex flex-col items-start max-w-screen-xl mx-auto w-full">
+        <Link
+          to={`/`}
+          className="mt-4 bg-white p-2 rounded-md text-left self-start"
+        >
+          Назад
+        </Link>
+        <div className="flex flex-wrap justify-center items-center gap-4 mt-20 mb-8 w-full">
+          {themes.map((theme) => (
+            <div
+              key={theme._id}
+              className="bg-white rounded-lg overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 shadow-md flex-grow"
+            >
+              <div className="p-4">
+                <Link
+                  to={`/sections/${param.section}/${theme.themeRoute}`}
+                  state={{themes: themes}}
+                  style={{
+                    alignSelf: "flex-start",
+                    textDecoration: "none",
+                  }}
+                  className="sm: ml-10"
+                >
+                  <h3 className="text-lg font-semibold">{theme.themeName}</h3>
+                </Link>
+              </div>
             </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Section;
--- client/src/components/Theme/index.jsx
+++ client/src/components/Theme/index.jsx
@@ -1,56 +1,59 @@
-import {useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link, useLocation } from "react-router-dom";
 import axios from "axios";
 import { replaceUnderscores } from "../../utils/repleceUnderscores";
 
 const Theme = () => {
-    const [themeName, setThemeName] = useState('');
-    const [article, setArticle] = useState([]);
-    const param = useParams();
+  const [themeName, setThemeName] = useState("");
+  const [article, setArticle] = useState([]);
+  
+  const param = useParams();
+  // const {state} = useLocation();
 
-    useEffect(() => {
-        const getTheme = async () => {
-        try {
-            const url = `http://localhost:4000/api/sections/${param.section}/${param.theme}`;
-            const { data } = await axios.get(url);
-            setArticle(data[0].info);
-            setThemeName(data[0].themeName)
-        } catch (error) {   
-            console.log(error);
-            setArticle([]);
-        }
-        };
-        getTheme();
-    },[param]);
+  // console.log(state.themes);
+  
+  useEffect(() => {
+    const getTheme = async () => {
+      try {
+        const url = `http://localhost:4000/api/sections/${param.section}/${param.theme}`;
+        const { data } = await axios.get(url);
+        setArticle(data[0].info);
+        setThemeName(data[0].themeName);
+      } catch (error) {
+        console.log(error);
+        setArticle([]);
+      }
+    };
+    getTheme();
+  }, [param]);
 
-    return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-            <h2 className="text-4xl font-semibold mx-auto mt-28">{themeName}</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto mt-20 mb-8"> 
+  return (
+    <div className="flex flex-col justify-center bg-gray-100">
+        <h2 className="text-4xl font-semibold mx-auto mt-28">{themeName}</h2>
+        <div className="flex flex-col items-start max-w-screen-xl mx-auto">
+            <Link
+                to={`/sections/${param.section}`}
+                className="mt-4 bg-white p-2 rounded-md text-left self-start"
+            >
+                Назад
+            </Link>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4 mb-8 w-full">
                 {article.map((item) => (
-                <div key={item._id} className="bg-white rounded-lg overflow-hidden shadow-md">
-                    <div className="p-8">
-                    <h3 className="text-xl">{replaceUnderscores(item.text, item.formulas)}</h3>
+                    <div
+                        key={item._id}
+                        className="bg-white rounded-lg overflow-hidden shadow-md"
+                    >
+                        <div className="p-8">
+                            <h3 className="text-xl">
+                                {replaceUnderscores(item.text, item.formulas)}
+                            </h3>
+                        </div>
                     </div>
-                </div>
                 ))}
             </div>
         </div>
-    );
-
-    // return (
-    //     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-    //         <div className="flex flex-wrap justify-center items-center gap-4 max-w-screen-xl mx-auto mt-28 mb-8">
-    //             {theme.map((item) => (
-    //                 <div key={item._id} className="bg-white rounded-lg overflow-hidden w-full sm:w-1/2 md:w-1/3 lg:w-1/4 shadow-md flex-grow">
-    //                     <div className="p-4">
-    //                         <h3 className="text-xl">{replaceUnderscores(item.text, item.formulas)}</h3>
-    //                     </div>
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     </div>
-    // );
-}
+    </div>
+  );
+};
 
 export default Theme;
