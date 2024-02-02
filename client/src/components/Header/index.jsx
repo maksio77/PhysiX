4acfebe939847fddba4e77eb197390fed367ec76
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-02-02 14:51:31 +0200

    header update

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,33 +1,80 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { TfiMenu } from "react-icons/tfi";
+import { FaRobot } from "react-icons/fa6";
+import { FaNoteSticky } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
 
 const Header = () => {
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        window.location.reload();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuOpen) setMenuOpen(false);
     };
 
-    return (
-        <>
-            <nav className="bg-primary p-4 w-full fixed top-0 z-10 flex justify-between shadow-md">
-                <Link
-                    to={'/'}
-                >
-                    <h1 className="text-4xl text-white">PhysiX</h1>
-                </Link>
-                <button className="bg-white text-primary px-4 py-2 rounded" onClick={handleLogout}>
-                    <Link
-                        to={'/login'}
-                        style={{
-                        alignSelf: "flex-start",
-                        textDecoration: "none",
-                        }}
-                    >
-                    Logout
-                    </Link>
-                </button>
-            </nav>
-        </>
-    )
-}
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, [menuOpen]);
+
+  const toggleMenu = (event) => {
+    event.stopPropagation();
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+  };
+
+  return (
+    <header className="bg-primary p-4 fixed top-0 w-full z-10 flex justify-between items-center shadow-md">
+      <div className="flex flex-start">
+        <button
+          onClick={toggleMenu}
+          className="bg-primary text-white px-4 py-2 rounded w-full hover:bg-secondary hover:text-primary"
+        >
+          <TfiMenu className="h-5 w-5" />
+        </button>
+      </div>
+      <Link to={"/"}>
+        <h1 className="sm: text-2xl lg:text-4xl text-white hover:text-secondary">PhysiX</h1>
+      </Link>
+      <button
+        onClick={handleLogout}
+        className="bg-white lg:text-xl sm: text-sm text-primary px-4 py-2 rounded hover:bg-secondary"
+      >
+        <Link
+          to={"/login"}
+          style={{ alignSelf: "flex-start", textDecoration: "none" }}
+        >
+          Logout
+        </Link>
+      </button>
+      {menuOpen && (
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="absolute top-full rounded-xl left-0 lg:w-64 bg-white z-20 flex flex-col p-4 items-center space-around space-y-4 transition-transform duration-200 transform translate-x-0 overflow-auto shadow-lg"
+        >
+          <button className="bg-primary text-white px-4 py-2 rounded w-full hover:bg-secondary hover:text-primary flex justify-between items-center">
+            <FaRobot className="self-start" size={20}/>
+            <span className="self-center flex-grow text-center">Симуляції</span>
+          </button>
+          <button className="bg-primary text-white px-4 py-2 rounded w-full hover:bg-secondary hover:text-primary flex justify-between items-center">
+            <FaNoteSticky size={20} className="self-start" />
+            <span className="self-center flex-grow text-center px-3">Тестування</span>
+          </button>
+          <button className="bg-primary text-white px-4 py-2 rounded w-full hover:bg-secondary hover:text-primary flex justify-between items-center">
+            <FaStar size={20} className="self-start" />
+            <span className="self-center flex-grow text-center">Обране</span>
+          </button>
+        </div>
+      )}
+    </header>
+  );
+};
 
 export default Header;
