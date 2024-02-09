36180282ced367a9f1922c3975aaa95639890ed4
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-02-09 12:07:33 +0200

    added fav demo

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -36,5 +36,29 @@ export const useHttp = () => {
     return await request('users/addPoints', 'POST', JSON.stringify({points}), headers);
   }, [request]);
 
+  // const addToFavorites = useCallback(async (testId, token) => {
+  //   const headers = {
+  //     "Content-Type": "application/json",
+  //     "x-access-token": token,
+  //   };
+  //   return await request(`users/favorites/add`, 'POST', JSON.stringify({testId}), headers);
+  // }, [request]);
+
+  // router.addFavorite = async (req, res) => {
+  //   try {
+  //     const { testId } = req.body;
+  //     const user = await User.findById(req.userId);
+    
+  //     if(!user.favorites.includes(testId)) {
+  //       user.favorites.push(testId);
+  //       await user.save();
+  //     }
+      
+  //     res.status(200).json({message: 'Test added to favorites'});
+  //   } catch (err) {
+  //     res.status(500).json({ message: 'Something went wrong. Please try again' });
+  //   }
+  // };
+
   return { loading, request, addPoints, error, clearError };
 };
