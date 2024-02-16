673e1eac1914b0683c2b57c743433cb83882814a
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-02-16 17:22:08 +0200

    added article and remove from favorite on front-end

--- client/src/components/PaginatedGrid/index.jsx
+++ client/src/components/PaginatedGrid/index.jsx
@@ -1,8 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { replaceUnderscores } from "../../utils/repleceUnderscores";
 import { GrLinkNext } from "react-icons/gr";
 import { GrLinkPrevious } from "react-icons/gr";
+import usePhysixService from "../../services/PhysixService";
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import Spinner from "../Spinner";
+import ErrorMessage from "../ErrorMessage";
 
 const itemsPerPage = 6;
 
@@ -11,8 +16,52 @@ const PaginatedGrid = ({ theme, searchPhrase, inputText }) => {
   let { page, section } = useParams();
   const pagesCount = theme.info.length / itemsPerPage;
 
+  const token = localStorage.getItem("token");
+  const {
+    loading,
+    error,
+    addFavoriteArticle,
+    removeFavoriteArticle,
+    getFavoriteArticlesIDS,
+  } = usePhysixService();
+
   const [currentPage, setCurrentPage] = useState(parseInt(page));
   const [isActive, setIsActive] = useState(true);
+  const [favoriteArticles, setFavoriteArticles] = useState([]);
+
+  const getFavorite = useCallback(async () => {
+    try {
+      const res = await getFavoriteArticlesIDS(token);
+      setFavoriteArticles(res);
+    } catch (error) {
+      console.error("Error fetching favorite tests:", error);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    getFavorite();
+  }, [getFavorite]);
+
+  const handleAddFavorite = async (article) => {
+    try {
+      await addFavoriteArticle(article, token);
+      getFavorite();
+    } catch (error) {
+      console.error("Error adding favorite test:", error);
+    }
+  };
+
+  const handleRemoveFavorite = async (articleID) => {
+    try {
+      await removeFavoriteArticle(articleID, token);
+      getFavorite();
+    } catch (error) {
+      console.error("Error removing favorite test:", error);
+    }
+  };
+
+  const errorMessage = error ? <ErrorMessage message={error} /> : null;
+  const spinner = loading ? <Spinner size={20} loading={loading} /> : null;
 
   const goToFirstPage = () => {
     setCurrentPage(1);
@@ -20,14 +69,11 @@ const PaginatedGrid = ({ theme, searchPhrase, inputText }) => {
   };
 
   useEffect(() => {
-    if (pagesCount + 1 <= currentPage || currentPage <=0)
-      goToFirstPage();  
+    if (pagesCount + 1 <= currentPage || currentPage <= 0) goToFirstPage();
 
     window.history.pushState(null, "", currentPage);
 
-    if (inputText && theme.info.length <=6)
-      goToFirstPage();
-
+    if (inputText && theme.info.length <= 6) goToFirstPage();
   }, [currentPage, inputText]);
 
   useEffect(() => {
@@ -76,6 +122,23 @@ const PaginatedGrid = ({ theme, searchPhrase, inputText }) => {
             } rounded-lg overflow-hidden shadow-md`}
           >
             <div className="p-8">
+              <button className="mt-4 bg-white shadow hover:bg-secondary hover:text-primary transition-all duration-200 ease-in-out text-primary sm:text-base p-2 text-right rounded-md mb-4">
+                {!errorMessage && !loading ? (
+                  favoriteArticles.includes(item._id) ? (
+                    <FaStar
+                      onClick={() => handleRemoveFavorite(item._id)}
+                      size={20}
+                    />
+                  ) : (
+                    <FaRegStar
+                      onClick={() => handleAddFavorite(item)}
+                      size={20}
+                    />
+                  )
+                ) : null}
+                {errorMessage}
+                {spinner}
+              </button>
               <div className="lg:text-xl">
                 {replaceUnderscores(item.text, item.formulas)}
               </div>
