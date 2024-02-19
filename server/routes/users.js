b348bfa8543571c2243c63f69feec2988723fe72
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-02-19 17:20:09 +0200

    added route user favorite items

--- server/routes/users.js
+++ server/routes/users.js
@@ -241,4 +241,20 @@ router.get("/currentUser", verifyToken, async (req, res) => {
   }
 });
 
+router.get("/userFavoriteItems", verifyToken, async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId).select("favoriteArticles favoriteTests");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: `Internal Server Error${error}` });
+  }
+});
+
 module.exports = router;
