1a6f7b8be911c03817b421169b5a1344a9e4e47b
Maksym Pavliv <maksiopavliv0@gmail.com>
2024-02-23 14:52:33 +0200

    footer update

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,10 +1,37 @@
+import { BsLinkedin } from "react-icons/bs";
+import { GrGithub } from "react-icons/gr";
+
 const Footer = () => {
   return (
-    <footer className="bg-primary py-12">
-      <div className="container mx-auto">
-        <p className="text-white text-center container">
+    <footer className="bg-primary py-8">
+      <div className="container mx-auto flex justify-center items-center flex-col">
+        <p className="text-white text-center">
           Copyright &copy; PhysiX 2024. All rights reserved.
         </p>
+        <div className="mt-2 flex space-x-4">
+          <div className="flex items-center">
+            <BsLinkedin className="mr-1 text-white" />
+            <a
+              href="https://www.linkedin.com/in/maksym-pavliv"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:underline"
+            >
+              LinkedIn
+            </a>
+          </div>
+          <div className="flex items-center ">
+            <GrGithub className="mr-1 text-white" />
+            <a
+              href="https://github.com/maksio77/PhysiX"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:underline"
+            >
+              GitHub
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
